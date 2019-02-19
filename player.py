c3d3b4eb81d4cdecb1b5f5f7bbfdf26583dfe93d
Bering <bering@ringlogic.com>
2019-02-19 06:06:00 -0500

    Better rotation control

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ class Player(GameObject):
 	def __init__(self):
 		super().__init__("player.png", 300, 300, 0, 0)
 		self.unrotated_surface = self.surface
-		self.rotation_speed = 10
+		self.rotation_speed = 0
 		self.angle = 0
 
 	def events(self, event):
@@ -16,12 +16,36 @@ class Player(GameObject):
 				self.rotate_right()
 			elif event.key == pygame.K_SPACE:
 				self.fire()
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
+				self.rotate_none()
+			elif event.key == pygame.K_RIGHT or event.key == pygame.K_KP6:
+				self.rotate_none()
+			elif event.key == pygame.K_SPACE:
+				self.fire_hold()
 
 	def rotate_left(self):
-		self.angle = self.clamp_angle(self.angle + self.rotation_speed)
+		self.rotation_speed = 5
 
 	def rotate_right(self):
-		self.angle = self.clamp_angle(self.angle - self.rotation_speed)
+		self.rotation_speed = -5
+
+	def rotate_none(self):
+		self.rotation_speed = 0
+
+	def fire(self):
+		pass
+
+	def fire_hold(self):
+		pass
+
+	def update(self, delta_time):
+		self.angle = self.clamp_angle(self.angle + self.rotation_speed)
+		self.surface = pygame.transform.rotate(self.unrotated_surface, self.angle)
+		new_rect = self.surface.get_rect()
+		new_rect.center = self.rect.center
+		self.rect = new_rect
+		super().update(delta_time)
 
 	def clamp_angle(self, angle):
 		while angle >= 360:
@@ -29,13 +53,3 @@ class Player(GameObject):
 		while angle < 0:
 			angle += 360
 		return angle
-
-	def fire(self):
-		pass
-
-	def update(self, delta_time):
-		self.surface = pygame.transform.rotate(self.unrotated_surface, self.angle)
-		new_rect = self.surface.get_rect()
-		new_rect.center = self.rect.center
-		self.rect = new_rect
-		super().update(delta_time)
