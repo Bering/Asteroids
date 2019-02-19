88c0e4da687aaaecee180ea744343fdb83a59e24
Bering <bering@ringlogic.com>
2019-02-18 21:17:34 -0500

    GameObject

---
+++ game_object.py
@@ -0,0 +1,16 @@
+import os
+
+class GameObject:
+
+	def __init__(self, image, x, y, vx, vy):
+		image_file = os.path.join("images", image)
+		self.surface = pygame.image.load(image_file).convert_alpha()
+		self.rect = self.surface.get_rect().move(x, y)
+		self.vx = vx
+		self.vy = vy
+
+	def update(delta_time):
+		self.rect.move(self.vx, self.vy)
+
+	def render(surface, rect):
+		surface.blit(self.surface, self.rect)
