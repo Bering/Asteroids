ba00112f86f65d94f6aa1dbf275cc4e19d6f59db
Bering <bering@ringlogic.com>
2019-02-20 08:39:28 -0500

    Build script ideas

--- build.py
+++ build.py
@@ -3,3 +3,7 @@
 
 import os
 os.system("pyinstaller --add-data Fonts/*.ttf;Fonts --add-data Images/*.png;Images --add-data Sounds/*.ogg;Sounds --noconsole __main__.py")
+
+# TODO: rename and zip the folder in the dist folder
+
+# TODO: also make a zip for the source
